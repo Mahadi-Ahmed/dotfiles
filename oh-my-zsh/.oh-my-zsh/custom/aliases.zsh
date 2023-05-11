38d9c002d0a6e7e0194adfa220620d35fb30c651
Mahadi-Ahmed <mahadia.dev@gmail.com>
2023-05-12 00:10:28 +0200

    add oh-my-zsh aliases

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,26 @@
+alias f='open -a Finder ./' #open current directory with finder
+alias subl="open -a /Applications/Sublime\ Text.app"
+
+# alias edit='lvim'
+# alias vim='lvim'
+
+alias edit='nvim'
+alias vim='nvim'
+
+alias cat='bat' 
+alias localsls='./node_modules/.bin/serverless'
+alias warp='open -n /Applications/Warp.app'
+alias cdpn='cd ~/Code/pn-repos/'
+alias cdmdf='cd ~/Code/pn-repos/system-components/modtagarflex/'
+alias cdqmk='cd ~/Code/mahadia/qmk/keyboards/planck/keymaps/mahadia'
+alias cpwd='pwd | pbcopy | echo "path copied to clipboard"'
+alias rsource='source ~/.zshrc'
+alias rc='vim ~/.zshrc'
+alias slslocal='localsls offline -s test'
+alias slslocaldev='localsls offline -s dev'
+alias slslocaltest='localsls offline -s test'
+# alias glg='git log -n 40 --graph --oneline --decorate'
+alias glg='git log -n 40 --graph --decorate'
+alias moveSc='/Users/mahadiahmed/Code/mahadia/Util-Scripts/screenshot_mover/screenshotMover.py'
+alias lg='lazygit'
+alias cdlv='cd /Users/mahadiahmed/.local/share/lunarvim/lvim'
