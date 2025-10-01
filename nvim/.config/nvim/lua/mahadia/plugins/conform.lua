fef74ae541e845be25f5aded7a454a056113446e
Mahadi-Ahmed <mahadia.dev@gmail.com>
2025-10-01 16:21:30 +0200

    [nvim] configure conform to auto-detect prettier and eslint configs per project
    Use require_cwd to only run formatters when config files exist,
     with LSP formatting as fallback when no formatter config is found

--- nvim/.config/nvim/lua/mahadia/plugins/conform.lua
+++ nvim/.config/nvim/lua/mahadia/plugins/conform.lua
@@ -3,13 +3,13 @@ local conform = require("conform")
 conform.setup({
 	formatters_by_ft = {
 		javascript = { "prettierd", "prettier", stop_after_first = true },
-		typescript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", "eslint_d", "eslint", stop_after_first = true },
 		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 		vue = { "prettierd", "prettier", stop_after_first = true },
 		lua = { "stylua", lsp_format = "fallback" },
 		html = { "prettierd", "prettier", stop_after_first = true },
-		ruby = { "rubyfmt", stop_after_first = true },
+		ruby = { "rubofmt", stop_after_first = true },
 		yaml = function(bufnr)
 			local filename = vim.api.nvim_buf_get_name(bufnr)
 			if filename:match("%.github/workflows/") then
@@ -18,8 +18,22 @@ conform.setup({
 				return { "prettierd", "prettier", lsp_format = "fallback", stop_after_first = true }
 			end
 		end,
-
 		go = { lsp_format = "fallback" },
+	},
+
+	formatters = {
+		prettierd = {
+			require_cwd = true,
+		},
+		prettier = {
+			require_cwd = true,
+		},
+		eslint_d = {
+			require_cwd = true,
+		},
+		eslint = {
+			require_cwd = true,
+		},
 	},
 
 	default_format_opts = {
