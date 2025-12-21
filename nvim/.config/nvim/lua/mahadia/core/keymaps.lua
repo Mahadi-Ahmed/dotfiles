2be9360b5c010da24784f12347b469c1c6d48fa2
mahadia <mahadia.dev@gmail.com>
2025-12-21 16:24:43 +0100

    [nvim] add file reference yanking keymaps

--- nvim/.config/nvim/lua/mahadia/core/keymaps.lua
+++ nvim/.config/nvim/lua/mahadia/core/keymaps.lua
@@ -52,6 +52,42 @@ keymap("n", "<leader>Y", [["+Y]])
 keymap({"n", "v"}, "<leader>p", [["+p"]])
 keymap({"n", "v"}, "<leader>P", [["+P"]])
 keymap("n", "<leader>ya", "gg\"*yG<C-o>", { desc = "Yank entire file", silent = false })
+
+-- Yank file path with line number reference
+local function get_relative_path()
+	-- Try to get path relative to git root first
+	local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+	if vim.v.shell_error == 0 and git_root then
+		local full_path = vim.fn.expand("%:p")
+		return full_path:gsub("^" .. vim.pesc(git_root) .. "/", "")
+	end
+	-- Fallback to cwd-relative path
+	return vim.fn.expand("%:.")
+end
+
+local function yank_file_reference()
+	local path = get_relative_path() .. ":" .. vim.fn.line(".")
+	vim.fn.setreg("+", path)
+	vim.notify("Yanked: " .. path, vim.log.levels.INFO)
+end
+
+local function yank_file_reference_range()
+	local start_line = vim.fn.line("v")
+	local end_line = vim.fn.line(".")
+	local lines = { start_line, end_line }
+	table.sort(lines)
+
+	local path = get_relative_path() .. ":" .. table.concat(lines, "-")
+	vim.fn.setreg("+", path)
+	vim.notify("Yanked: " .. path, vim.log.levels.INFO)
+
+	-- Exit visual mode
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+end
+
+keymap("n", "<leader>yf", yank_file_reference, { desc = "Yank file:line reference", silent = true })
+keymap("v", "<leader>yf", yank_file_reference_range, { desc = "Yank file:line-line reference", silent = true })
+
 keymap('n', 's', '<Nop>', { noremap = true, silent = true })
 
 -- vnoremap  <leader>y  "+y
