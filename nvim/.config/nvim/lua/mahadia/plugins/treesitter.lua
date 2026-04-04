31458882dc30e81310864ecc78c04d96ceaa58c5
mahadia <mahadia.dev@gmail.com>
2026-04-04 13:04:38 +0200

    [nvim] add more treesitter languages

--- nvim/.config/nvim/lua/mahadia/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mahadia/plugins/treesitter.lua
@@ -1,39 +1,44 @@
 local status, treesitter = pcall(require, "nvim-treesitter")
 if not status then
-  return
+	return
 end
 
 treesitter.setup({
-  install_dir = vim.fn.stdpath("data") .. "/site",
+	install_dir = vim.fn.stdpath("data") .. "/site",
 })
 
 -- Install parsers (replaces ensure_installed)
 treesitter.install({
-  "json",
-  "javascript",
-  "typescript",
-  "tsx",
-  "yaml",
-  "html",
-  "css",
-  "scss",
-  "markdown",
-  "bash",
-  "lua",
-  "vim",
-  "dockerfile",
-  "gitignore",
-  "vue",
-  "python",
-  "regex",
-  "ruby",
+	"json",
+	"javascript",
+	"typescript",
+	"tsx",
+	"yaml",
+	"html",
+	"css",
+	"scss",
+	"markdown",
+	"markdown_inline",
+	"bash",
+	"lua",
+	"vim",
+	"dockerfile",
+	"gitignore",
+	"vue",
+	"python",
+	"regex",
+	"ruby",
+	"go",
+	"gitcommit",
+	"scss",
+	"toml",
 })
 
 -- Enable treesitter highlighting for filetypes with installed parsers.
 -- Neovim 0.12 only auto-enables for lua, markdown, help, query.
 -- Replaces `highlight = { enable = true }` from the old master branch.
 vim.api.nvim_create_autocmd("FileType", {
-  callback = function()
-    pcall(vim.treesitter.start)
-  end,
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
 })
