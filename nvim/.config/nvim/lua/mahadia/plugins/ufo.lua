3845cba86b58510e4175e95c049dd8c75459e041
mahadia <mahadia.dev@gmail.com>
2026-04-27 10:33:34 +0200

    [nvim] fix ufo folds
    https://github.com/kevinhwang91/nvim-ufo#customize-fold-text

--- nvim/.config/nvim/lua/mahadia/plugins/ufo.lua
+++ nvim/.config/nvim/lua/mahadia/plugins/ufo.lua
@@ -7,8 +7,42 @@ end
 vim.keymap.set('n', 'zR', ufo.openAllFolds)
 vim.keymap.set('n', 'zM', ufo.closeAllFolds)
 
+local function foldtext(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = ('  %d lines'):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      table.insert(newVirtText, { chunkText, chunk[2] })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+
+  local peek = vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, false)[1]
+  if peek and peek:match('%S') then
+    table.insert(newVirtText, { '  ' .. peek:match('^%s*(.-)%s*$'), 'Comment' })
+  end
+
+  table.insert(newVirtText, { suffix, 'MoreMsg' })
+  return newVirtText
+end
+
 ufo.setup({
   provider_selector = function(bufnr, filetype, buftype)
     return { 'treesitter', 'indent' }
-  end
+  end,
+  fold_virt_text_handler = foldtext,
 })
